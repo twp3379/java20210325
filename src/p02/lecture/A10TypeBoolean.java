b81f832e6db60e86af946d21722d4d237fcf9120
twp3379 <wkakzks@naver.com>
2021-03-26 16:28:02 +0900

    2021.03.26 p03

---
+++ src/p02/lecture/A10TypeBoolean.java
@@ -0,0 +1,14 @@
+package p02.lecture;
+
+public class A10TypeBoolean {
+	public static void main(String[] args) {
+		// bloolean : 1byte (8bit) 논리형 저장
+		// 값: ture, false
+		boolean var1;
+		var1 = true;
+		System.out.println(var1);
+
+		var1 = false;
+		System.out.println(var1);
+	}
+}
