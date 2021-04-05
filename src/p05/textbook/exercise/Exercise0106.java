810639e8f782d0b9c2ade664769bcb3737485e78
twp3379 <wkakzks@naver.com>
2021-04-05 15:31:21 +0900

    2021.04.05

---
+++ src/p05/textbook/exercise/Exercise0106.java
@@ -0,0 +1,22 @@
+package p05.textbook.exercise;
+
+public class Exercise0106 {
+public static void main(String[] args) {
+	 // 1번 문제 :
+	 // 답 : 4번
+	
+	 // 2번 문제 :
+	 // 답 : 3번
+	
+	 // 3번 문제 :
+	 // 답 : 2번
+	
+	 // 4번 문제 :
+	 // 답 : 2번
+	int[] array;
+	array = new int[] { 1, 2, 3};
+	
+	 // 5번 문제 :
+	 // 답 : 3번 (boolean false)
+}
+}
