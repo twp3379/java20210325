dfe1bb0e6bd2e5be376d993376f3bc61a0515e34
twp3379 <wkakzks@naver.com>
2021-04-07 15:32:34 +0900

    2021.04.07

---
+++ src/p06/lecture/p4method/A03Thiskeyword.java
@@ -0,0 +1,11 @@
+package p06.lecture.p4method;
+
+public class A03Thiskeyword {
+	public static void main(String[] args) {
+		MyClass3 c1 = new MyClass3();
+		c1.method1();
+//		c1.method2();
+
+	}
+
+}
