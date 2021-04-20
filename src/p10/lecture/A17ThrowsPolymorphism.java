3d4247af39124f9298b406b6565d700e4da61efa
twp3379 <wkakzks@naver.com>
2021-04-20 20:10:36 +0900

    2021.04.20

---
+++ src/p10/lecture/A17ThrowsPolymorphism.java
@@ -0,0 +1,16 @@
+package p10.lecture;
+
+import java.io.FileInputStream;
+import java.io.FileNotFoundException;
+
+public class A17ThrowsPolymorphism {
+	public static void main(String[] args) throws Exception {
+		method1();
+	}
+	
+	private static void method1() throws Exception {
+		Class.forName("java.lang.String");
+		FileInputStream file = new FileInputStream("");
+	}
+
+}
