60b8194228d803aa40a78e71cc27394882b11aec
twp3379 <wkakzks@naver.com>
2021-04-29 16:10:09 +0900

    2021.04.29

---
+++ src/p15/lecture/sample/Ex07Map.java
@@ -0,0 +1,41 @@
+package p15.lecture.sample;
+
+import java.util.HashMap;
+import java.util.Map;
+
+public class Ex07Map {
+	public static void main(String[] args) {
+		int[] arr1 = { 1, 1, 1, 3, 3, 2, 3, 2, 1, 4, 5, 5 };
+		
+		Map<Integer, Integer> map = count(arr1);
+
+		for (Map.Entry<Integer, Integer> entry : map.entrySet()) {
+			System.out.println(entry.getKey() + ":" + entry.getValue());
+		}
+		
+		/*
+			3:3
+			5:2
+			1:4
+			2:2
+			4:1
+
+		 */
+	}
+
+	private static Map<Integer, Integer> count(int[] arr1) {
+		Map<Integer, Integer> map = new HashMap<>();
+		
+		for (int num : arr1) {
+			if (map.containsKey(num)) {
+//				int cnt = map.get(num);
+//				map.put(num, cnt+1);
+				map.put(num, map.get(num) + 1);
+			} else {
+				map.put(num, 1);
+			}
+		}
+		
+		return map;
+	}
+}
--- src/p15/textbook/StackExample.java
+++ src/p15/textbook/StackExample.java
@@ -1,23 +1,28 @@
-package p15.textbook.exercises.ex07;
+package p15.textbook;
 
 import java.util.Stack;
 
-import p15.textbook.Coin;
-
 public class StackExample {
 	public static void main(String[] args) {
-		Stack<Coin> coinBox = new Stack<Coin>();
+		Stack<Coin> coinBox = new Stack<>();
 		
 		coinBox.push(new Coin(100));
 		coinBox.push(new Coin(50));
 		coinBox.push(new Coin(500));
 		coinBox.push(new Coin(10));
 		
-		while(!coinBox.isEmpty()) {
+		while (!coinBox.isEmpty()) {
 			Coin coin = coinBox.pop();
-			System.out.println("꺼내온 동전" + coin.getValue() +"원");
+			System.out.println("꺼내온 동전 : " + coin.getValue() + "원");
 		}
-	 
 	}
-
 }
+
+
+
+
+
+
+
+
+
