5563c53ef5d21b538cf13cbaf014f02043276f4c
twp3379 <wkakzks@naver.com>
2021-05-04 16:19:14 +0900

    2021.05.04

---
+++ src/p18/lecture/network/A01Network.java
@@ -0,0 +1,13 @@
+package p18.lecture.network;
+
+public class A01Network {
+	// win + r , cmd
+	// internet protocal address (ip address)
+	// port number 0 ~ 65535 (0~1023 x)
+	
+	// socket
+	
+	
+	
+
+}
---
+++ src/p18/textbook/BufferedReaderExample.java
@@ -0,0 +1,21 @@
+package p18.textbook;
+
+import java.io.BufferedReader;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+import java.io.Reader;
+
+public class BufferedReaderExample {
+	public static void main(String[] args) throws Exception{
+		InputStream is = System.in;
+		Reader reader = new InputStreamReader(is);
+		// 여러번 감쌀수 있다.
+		BufferedReader br = new BufferedReader(reader);
+		
+		System.out.println("입력: ");
+		String lineString = br.readLine();
+		
+		System.out.println("출력: " + lineString);
+	}
+
+}
